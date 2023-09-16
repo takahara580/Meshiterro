de759dcaa4778f763d9e7c7041be4c8f38fd8df9
EC2 Default User <ec2-user@ip-172-31-42-207.ap-northeast-1.compute.internal>
2023-09-16 00:03:33 +0000

    [Finish] 4-19

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :edit, :update]
